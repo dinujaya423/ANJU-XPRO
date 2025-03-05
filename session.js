ee31a36c50069ca857dc9116a24000a4e2bf9dec
dinujaya423 <dinujaya423@gmail.com>
2025-03-05 18:44:14 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~uAdnnLpB#QYjcJxEINdb3fsHCihuWGL6sItLwHLrH3DThPlpkS7E",
+SUDO: process.env.SUDO || ["94774674713","94774674713"],
 PORT: process.env.PORT || "8000"
 };
